8e7fb9400d2ea55e9824ae2f60fc54a74d84a6a9
Kittaka <jdavisson87@gmail.com>
2020-03-06 14:43:01 -0600

    Integrated Firebase
    Integrated Firebase into application, created a utils folder, created
    a sign in page with sign in component and sign in with google.  Also,
    adjusted the header to show sign in or sign out, and adjusted styling.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,13 +50,14 @@ class SignIn extends React.Component{
             handleChange={this.handleChange}
             required 
           />
-
-          <CustomButton type='submit' value='Submit Form' > Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {' '}
-            Sign in with Google 
-            {' '}
-          </CustomButton>
+            <div className='buttons'>
+              <CustomButton type='submit' value='Submit Form' > Sign In </CustomButton>
+              <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                {' '}
+                Sign in with Google 
+                {' '}
+              </CustomButton>
+            </div>
           </form>
       </div>
     )
