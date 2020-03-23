2e7e24641456d52710137370e2c78fce8e1b76f2
Kittaka <jdavisson87@gmail.com>
2020-03-22 19:10:17 -0500

    updated shop info into firebase database.  Redux now calls async function to populate the shop state

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,41 +3,73 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDdQruw7OFLtPN2G_ccrpJbqc0M28d9g-U",
-  authDomain: "ecommerce-ade64.firebaseapp.com",
-  databaseURL: "https://ecommerce-ade64.firebaseio.com",
-  projectId: "ecommerce-ade64",
-  storageBucket: "ecommerce-ade64.appspot.com",
-  messagingSenderId: "389008601963",
-  appId: "1:389008601963:web:3be7f6fd1d766ce68f922e",
-  measurementId: "G-CTEQ3WRGJB"
+  apiKey: 'AIzaSyDdQruw7OFLtPN2G_ccrpJbqc0M28d9g-U',
+  authDomain: 'ecommerce-ade64.firebaseapp.com',
+  databaseURL: 'https://ecommerce-ade64.firebaseio.com',
+  projectId: 'ecommerce-ade64',
+  storageBucket: 'ecommerce-ade64.appspot.com',
+  messagingSenderId: '389008601963',
+  appId: '1:389008601963:web:3be7f6fd1d766ce68f922e',
+  measurementId: 'G-CTEQ3WRGJB'
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get()
+  const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-    
+
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
-      console.log('error creating user', error.message)
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
-}
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(object => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, object);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 firebase.initializeApp(config);
 
@@ -46,6 +78,6 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
