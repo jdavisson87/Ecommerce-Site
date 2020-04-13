dcf1152f295c61e732e4865cf7a0e9cd7348aa8f
Kittaka <jdavisson87@gmail.com>
2020-04-13 16:51:51 -0500

    Fixing bug in server code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression);
+  app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
+  console.log('Server running on port ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
